7d4d82bf01f2594bfaef771b00f1e0f952354960
Chinna Sakthi k <75113031+Sakthisaba@users.noreply.github.com>
2025-07-22 11:13:26 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,9 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 
-app.listen((PORT_NO), () => {
-        console.log(`Server is running on port ${PORT_NO}`);
-    })
+app.listen()
 
       
 app.get('/', )
